62c4e02182582971c859e13ef05a3e6fdb67b260
MrPapr <luca.zettl@gmx.at>
2026-09-24 10:08:31 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='simplicissimus-offline-v2',
+const C='simplicissimus-offline-v2.1',
 A=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./simp-logo.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
