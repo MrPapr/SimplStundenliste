4ca4226b1ba0aff0a5d79595a315cb7c5790e343
MrPapr <luca.zettl@gmx.at>
2026-09-25 10:49:34 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
                     </div>
                 </header>
 
-                <div class="status">● Offline-App · V1.1.7</div>
+                <div class="status">● Offline-App · V1.1.8</div>
 
                 <nav>
                     <button data-v="day" class="active">Tag</button>
